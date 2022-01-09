2b4539777cdeaca4b44674a4ca5e0b8933f90fdb
Md. Mizanur Rahman Ashiq <mizanur35-2844@diu.edu.bd>
2022-01-09 17:05:19 +0600

    initial commit -m :tada:

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+
+const connectDatabase = require('./config/database');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDatabase();
+
+const jobs = require('./routes/jobs')
+
+app.use(jobs);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+	console.log(`Server stated on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+});
---
+++ controllers/jobsController.js
@@ -0,0 +1,6 @@
+exports.getJobs = (req, res, next) => {
+    res.status(200).json({
+        success: true,
+        message: 'This route will display all jobs near future'
+    })
+}

610a3fab52a18f9e0f3cd451bdde758e8d9a24a8
Md. Mizanur Rahman Ashiq <mizanur35-2844@diu.edu.bd>
2022-09-08 11:18:28 +0600

    slug added to job

--- models/jobs.model.js
+++ models/jobs.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const jobSchema = new mongoose.Schema({
     title: {
@@ -23,6 +24,22 @@ const jobSchema = new mongoose.Schema({
     address: {
         type: String,
         required: [true, 'job adress is required.'],
+    },
+    location: {
+        type: {
+            type: String,
+            enum: ['Point'],
+        },
+        coordinates: {
+            type: [Number],
+            index: '2dsphere',
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String,
     },
     company: {
         type: String,
@@ -83,9 +100,23 @@ const jobSchema = new mongoose.Schema({
 });
 
 // create job slug from the title
-jobSchema.pre('save', function(next) {
+jobSchema.pre('save', function (next) {
     this.slug = slugify(this.title, { lower: true });
     next();
+});
+
+jobSchema.pre('save', async function (next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode,
+    };
 });
 
 module.exports = mongoose.model('Job', jobSchema);

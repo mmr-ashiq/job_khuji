5eec7be0a1e9796d663f5dec8c5b3f7f8a1ee9d4
unknown <mizanur35-2844@diu.edu.bd>
2022-09-11 01:52:56 +0600

    update & delete job controller added

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -3,48 +3,85 @@ const geoCoder = require('../utils/geocoder');
 
 // create all job => /api/v1/jobs
 exports.getJobs = async (req, res, next) => {
+	const jobs = await Job.find();
 
-    const jobs = await Job.find();
-
-    res.status(200).json({
-        success: true,
-        results: jobs.length,
-        data: jobs
-    });
+	res.status(200).json({
+		success: true,
+		results: jobs.length,
+		data: jobs,
+	});
 };
 
 // create a new job => /api/v1/job/new
 exports.newJob = async (req, res, next) => {
-    const job = await Job.create(req.body);
+	const job = await Job.create(req.body);
 
-    res.status(200).json({
-        success: true,
-        message: 'job created',
-        data: job,
-    });
+	res.status(200).json({
+		success: true,
+		message: 'job created',
+		data: job,
+	});
 };
 
 // Search job with redius => /api/v1/jobs/:zipcode/:distance
 exports.getJobsInRadius = async (req, res, next) => {
-    const { zipcode, distance } = req.params;
+	const { zipcode, distance } = req.params;
 
-    // get lat/lng from geocoder
-    const loc = await geoCoder.geocode(zipcode);
-    const lat = loc[0].latitude;
-    const lng = loc[0].longitude;
+	// get lat/lng from geocoder
+	const loc = await geoCoder.geocode(zipcode);
+	const lat = loc[0].latitude;
+	const lng = loc[0].longitude;
 
-    // calc radius using radians
-    // divide distance by radius of Earth
-    // Earth Radius = 3,963 mi / 6,378 km
-    const radius = distance / 3963;
+	// calc radius using radians
+	// divide distance by radius of Earth
+	// Earth Radius = 3,963 mi / 6,378 km
+	const radius = distance / 3963;
 
-    const jobs = await Job.find({
-        location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
-    });
+	const jobs = await Job.find({
+		location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+	});
+
+	res.status(200).json({
+		success: true,
+		results: jobs.length,
+		data: jobs,
+	});
+};
+
+// update job => /api/v1/job/:id
+exports.updateJob = async (req, res, next) => {
+	const job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
+
+	if (!job) {
+		return res.status(404).json({
+			success: false,
+			message: 'job not found',
+		});
+	}
+
+	res.status(200).json({
+		success: true,
+		message: 'job updated',
+		data: job,
+	});
+};
+
+// delete job => /api/v1/job/:id
+exports.deleteJob = async (req, res, next) => {
+    const job = await Job.findByIdAndDelete(req.params.id);
+
+    if (!job) {
+        return res.status(404).json({
+            success: false,
+            message: 'job not found',
+        });
+    }
 
     res.status(200).json({
         success: true,
-        results: jobs.length,
-        data: jobs,
+        message: 'job deleted',
     });
-};
+}
--- routes/jobs.routes.js
+++ routes/jobs.routes.js
@@ -5,11 +5,17 @@ const {
     getJobs,
     newJob,
     getJobsInRadius,
+    updateJob,
+    deleteJob,
  } = require('../controllers/jobs.controller');
 
 router.route('/jobs').get(getJobs);
 router.route('/jobs/:zipcode/:distance').get(getJobsInRadius);
 
 router.route('/job/new').post(newJob);
+
+router.route('/job/:id').put(updateJob);
+
+router.route('/job/:id').delete(deleteJob);
 
 module.exports = router;

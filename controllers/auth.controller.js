de35dba268a3b0b036ef9d70629d6a08358d422e
Md. Mizanur Rahman Ashiq <mizanur35-2844@diu.edu.bd>
2023-02-18 17:03:17 +0600

    add reset password token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,9 +3,11 @@ const catchAsync = require('../middlewares/asyncErrors');
 const ErrorHandler = require('../utils/errorHandler');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
 exports.signup = catchAsync(async (req, res, next) => {
-	const { userName, fullName, email, role, password, confirmPassword } = req.body;
+	const { userName, fullName, email, role, password, confirmPassword } =
+		req.body;
 
 	const newUser = await User.create({
 		userName,
@@ -52,7 +54,9 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
 	await user.save({ validateBeforeSave: false });
 
-	const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`;
+	const resetUrl = `${req.protocol}://${req.get(
+		'host'
+	)}/api/v1/reset-password/${resetToken}`;
 
 	const message = `Please click on the link to reset your password: ${resetUrl}\n The link will expire in 10 minutes.`;
 
@@ -75,4 +79,38 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
 		return next(new ErrorHandler(error.message, 500));
 	}
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+	const resetPasswordToken = crypto
+		.createHash('sha256')
+		.update(req.params.token)
+		.digest('hex');
+
+	const user = await User.findOne({
+		passwordResetToken: resetPasswordToken,
+		passwordResetExpires: { $gt: Date.now() },
+	});
+
+	if (!user) {
+		return next(
+			new ErrorHandler(
+				'Password reset token is invalid or has been expired',
+				400
+			)
+		);
+	}
+
+	if (req.body.password !== req.body.confirmPassword) {
+		return next(new ErrorHandler('Password does not match', 400));
+	}
+
+	user.password = req.body.password;
+	user.confirmPassword = req.body.confirmPassword;
+	user.passwordResetToken = undefined;
+	user.passwordResetExpires = undefined;
+
+	await user.save();
+
+	sendToken(user, 200, res);
 });
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    signup,
-    login,
-    forgotPassword,
+	signup,
+	login,
+	forgotPassword,
+	resetPassword,
 } = require('../controllers/auth.controller');
 
 router.route('/signup').post(signup);
 router.route('/login').post(login);
 router.route('/forgot-password').post(forgotPassword);
+router.route('/reset-password/:token').put(resetPassword);
 
 module.exports = router;
